d686b7de9b1b70a9826179eb5dd2247781c5978b
cicdguy <26552821+cicdguy@users.noreply.github.com>
2022-08-22 12:27:25 -0500

    No XML reporting

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,8 +2,7 @@ pkg_name <- "tern.gee"
 if (requireNamespace("testthat", quietly = TRUE)) {
   library(testthat)
   reporter <- MultiReporter$new(list(
-    CheckReporter$new(),
-    JunitReporter$new(file = "junit-result.xml")
+    CheckReporter$new()
   ))
   test_results <- test_check(pkg_name, reporter = reporter)
   saveRDS(test_results, "unit_testing_results.rds")
