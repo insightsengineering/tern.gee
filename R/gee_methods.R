dd5e7e04fc6aa3bffba737c17724db46ce609368
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2022-10-21 10:03:40 +0200

    add the VarCorr part

--- R/gee_methods.R
+++ R/gee_methods.R
@@ -7,12 +7,33 @@ NULL
 #' @rdname gee_methods
 #' @importFrom nlme VarCorr
 #' @exportS3Method
-VarCorr.tern_gee <- function(x, sigma = x$scale, ...) {
-  # todo - need to figure out how to build the matrix from
-  # x$geese$alpha and the x$corstr etc - maybe just 4 different cases
-  # and build the matrix each time.
+VarCorr.tern_gee <- function(x, sigma = 1, ...) {
+  dim_mat <- length(x$visit_levels)
+  tmp <- id_mat <- diag(dim_mat)
+  corest <- x$geese$alpha
 
-  # mat <- x$working.correlation * sigma^2
-  # rownames(mat) <- colnames(mat) <- x$visit_levels
-  # mat
+  # Start with lower-triangular matrix part.
+  lower_mat <- switch(
+    x$corstr,
+    unstructured = , # Since this is the same as exchangeable, we can do this.
+    exchangeable = {
+      tmp[lower.tri(tmp)] <- corest
+      tmp
+    },
+    ar1 = {
+      row_col_diff <- row(tmp) - col(tmp)
+      tmp[lower.tri(tmp)] <- corest^(row_col_diff[lower.tri(row_col_diff)])
+      tmp
+    },
+    `m-dependent` = {
+      row_col_diff <- row(tmp) - col(tmp)
+      tmp[lower.tri(tmp)] <- corest[row_col_diff[lower.tri(row_col_diff)]]
+      tmp
+    }
+  )
+
+  # Construct the full symmetric matrix.
+  mat <- lower_mat + t(lower_mat) - id_mat
+  rownames(mat) <- colnames(mat) <- x$visit_levels
+  mat
 }
--- tests/testthat/test-gee_methods.R
+++ tests/testthat/test-gee_methods.R
@@ -10,6 +10,50 @@ test_that("vcov works as expected", {
   expect_matrix(result, nrows = 4, ncols = 4)
 })
 
-test_that("VarCorr works as expected", {
+test_that("VarCorr works as expected for unstructured correlation structure", {
+  model <- fit_gee(
+    vars = fev_vars,
+    data = fev_data,
+    regression = "logistic",
+    cor_struct = "unstructured"
+  )
+
   result <- expect_silent(VarCorr(model))
+  expect_true(all(eigen(result)$values > 0))
+})
+
+test_that("VarCorr works as expected for compound symmetry correlation structure", {
+  model <- fit_gee(
+    vars = fev_vars,
+    data = fev_data,
+    regression = "logistic",
+    cor_struct = "compound symmetry"
+  )
+
+  result <- expect_silent(VarCorr(model))
+  expect_true(all(eigen(result)$values > 0))
+})
+
+test_that("VarCorr works as expected for AR1 correlation structure", {
+  model <- fit_gee(
+    vars = fev_vars,
+    data = fev_data,
+    regression = "logistic",
+    cor_struct = "auto-regressive"
+  )
+
+  result <- expect_silent(VarCorr(model))
+  expect_true(all(eigen(result)$values > 0))
+})
+
+test_that("VarCorr works as expected for Toeplitz correlation structure", {
+  model <- fit_gee(
+    vars = fev_vars,
+    data = fev_data,
+    regression = "logistic",
+    cor_struct = "toeplitz"
+  )
+
+  result <- expect_silent(VarCorr(model))
+  expect_true(all(eigen(result)$values > 0))
 })
