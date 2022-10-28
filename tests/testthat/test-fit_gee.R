1ef14fa82133e0641710a7de2c90cfd8d42a240b
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2022-10-28 14:25:58 -0400

    Add tests for build functions

--- tests/testthat/test-fit_gee.R
+++ tests/testthat/test-fit_gee.R
@@ -1,9 +1,40 @@
 # build_formula ----
 
+test_that("build_formula builds the correct formula", {
+  vars <- list(response = "AVAL", id = "USUBJID", arm = "ARMCD", visit = "AVISIT", covariates = c("RACE", "SEX"))
+  result <- as.character(build_formula(vars))
+  expected <- as.character(AVAL ~ ARMCD + RACE + SEX)
+  expect_identical(result, expected)
+})
+
 test_that("build_formula works without covariates", {
   result <- expect_silent(build_formula(vars_gee()))
   expected <- AVAL ~ ARM
   expect_equal(result, expected, ignore_attr = TRUE)
+})
+
+# build_family ----
+
+test_that("build_family returns the correct class", {
+  result <- as.character(build_family("logistic"))[2]
+  expected <- "tern_gee_logistic"
+  expect_identical(result, expected)
+})
+
+test_that("build_family gives error message if incorrect regression type is given", {
+  expect_error(build_family("BLAHHH"), "regression type BLAHHH not supported")
+})
+
+# build_cor_details ----
+
+test_that("build_cor_details returns the correct correlation details", {
+  result <- as.character(build_cor_details("unstructured", fev_vars, fev_data))
+  expected <- c("unstructured", "1")
+  expect_identical(result, expected)
+})
+
+test_that("build_cor_details gives error message if incorrect correlation structure is given", {
+  expect_error(build_cor_details("BLAHHH", fev_vars, fev_data), "correlation structure BLAHHH not available")
 })
 
 # fit_gee ----
